db6d258a2d89dd4db05b9a3c0c82d8f17326a6d1
microvac23 <ithanmachuca@gmail.com>
2023-09-02 23:28:39 -0500

    file setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
-import Card from './components/Card';
+import Project from './components/Project';
+import Footer from './components/Footer';
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <div>
       <Header />
       <Navbar />
-      <Card />
+      <Project />
+      <Footer />
     </div>
   );
 }

390e38422df31ed17cdca99ad482ef3d77d4fa3d
microvac23 <Ithanmachuca@gmail.com>
2023-09-14 16:47:10 -0500

    photofix

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,7 +9,7 @@ export default function About() {
       <h1 style={{textAlign:'center'}}>About Me</h1>
       <img style={{
         maxWidth: 200,
-      }} alt='selfie' src='./images/selfie.jpg'></img>
+      }} alt='selfie' src='./Images/selfie.jpg'></img>
       <p>
         Hello,<br></br>
         My name is Amadeus Machuca. I am 23 years old and currently transitioning into a career of fullstack web development. I have worked 6 years in the service industry and worked as a line cook, baker, pastry chef, and now a bartender. I always had an attraction towards computers and information science so I am currently in Northwestern's Fullstack Webdevelopment Bootcamp. I am expected to graduate in Sept 2023!
